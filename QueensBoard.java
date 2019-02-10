d7860691cb2b412d52a29bc3bce7dd2fda3520d8
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-10 08:45:54 -0500

    added exception in countSolutions

--- QueensBoard.java
+++ QueensBoard.java
@@ -27,33 +27,26 @@ public class QueensBoard{
       board[r][c] = -1;}
       }
 
-  public int count(){
-    int counter = 0;
-    for(int i = 0; i < board.length; i++){
-      for(int j = 0; j < board.length; j++){
-        if(board[i][j] == -1){
-          counter ++;}}}
-    return counter;}
+
     public int countSolutions(){
-       return countSolutionsH(0, 0, 0, 0);
+      for(int i = 0; i < board.length; i++){
+        for(int j = 0; j < board.length; j++){
+          if(board[i][j] != 0){
+            throw new IllegalStateException("please make board empty");}}}
+       return countSolutionsH(0, 0, 0, 0, 0);
      }
      public int find(int r, int c){
        for(int i = 0; i < board.length; i++){
          if(board[i][c] == -1){
            return i;}}
       return -1;}
-     public int countSolutionsH(int r, int c, int counter, int startOff){
-       if(count() == board.length){
+     public int countSolutionsH(int r, int c, int counter, int startOff, int placed){
+       if(placed == board.length){
          // if all queens
          edit(startOff, c-1, false);
          // remove queens in last and previous column
          // move queen down one in previous column
-         try{
-         return countSolutionsH(startOff + 1, c - 1, counter += 1, find(r, c-2));}
-         catch(StackOverflowError e){
-           return countSolutionsH(startOff + 1, c - 1, counter += 1, find(r, c-2));
-         }
-       }
+         return countSolutionsH(startOff + 1, c - 1, counter += 1, find(r, c-2), 0);}
        if(r == board.length && c == 0){
          return counter;}
        if(r >= board.length){
@@ -62,20 +55,20 @@ public class QueensBoard{
          // clear previous column
          //try adding to next row of that column
          if(c >= 2){
-           return countSolutionsH(startOff + 1, c - 1, counter, find(r, c - 2));}
+           return countSolutionsH(startOff + 1, c - 1, counter, find(r, c - 2), placed - 1);}
 
          if(c == 1){
-           return countSolutionsH(startOff + 1, 0, counter, 0);}}
+           return countSolutionsH(startOff + 1, 0, counter, 0, placed - 1);}}
        if(board[r][c] == 0){
          // if no intersections add queen
          edit(r, c, true);
-         return countSolutionsH(0, c  + 1, counter, r);}
-       return countSolutionsH(r + 1, c, counter, startOff);}
+         return countSolutionsH(0, c  + 1, counter, r, placed + 1);}
+       return countSolutionsH(r + 1, c, counter, startOff, placed);}
 
-  public boolean solveH(int r, int c){
+  public boolean solveH(int r, int c, int placed){
     // records queen position of previous column
     int startOff = 0;
-    if(count() == board.length){
+    if(placed == board.length){
       // if all queens are on board return true
       return true;}
     if(c >= board.length){
@@ -89,17 +82,17 @@ public class QueensBoard{
         // remove that queen
       edit(startOff, c - 1, false);
       // try putting a queen directly below previous position
-      return solveH(startOff + 1, c - 1);}
+      return solveH(startOff + 1, c - 1, placed - 1);}
     if(board[r][c] == 0){
       // if no intersections in queens' paths
       edit(r, c, true);
       // add queen and move to next column
-      return solveH(0, c  + 1);}
+      return solveH(0, c  + 1, placed + 1);}
       // else try adding queen in next row
-    return solveH(r + 1, c);}
+    return solveH(r + 1, c, placed);}
 
   public boolean solve(){
-    return solveH(0, 0);}
+    return solveH(0, 0, 0);}
 
   public String toString(){
     String output = "";
