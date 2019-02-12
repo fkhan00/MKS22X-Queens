b4fffeb3458bcf30a5bdee7eb4f346a7358edacd
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-11 19:40:06 -0500

    found better algorithm for countSolutions
    can go up to a 14 by 14 board and its helper only takes in one parameter.  I made the helper a void function and had it add to a field variable that keeps track of the number of solutions.

--- QueensBoard.java
+++ QueensBoard.java
@@ -1,9 +1,13 @@
 public class QueensBoard{
   private int[][] board;
+  private int counter;
   public QueensBoard(int n){
-    board = new int[n][n];}
+    board = new int[n][n];
+    counter = 0;}
   public void edit(int r, int c, boolean add){
     int output = -1;
+    if(! add && board[r][c] != -1){
+      return;}
     if(add){
       output = 1;}
     for(int i = 0; i < board.length; i++){
@@ -26,44 +30,27 @@ public class QueensBoard{
     if(add){
       board[r][c] = -1;}
       }
-
-
-    public int countSolutions(){
-      for(int i = 0; i < board.length; i++){
-        for(int j = 0; j < board.length; j++){
-          if(board[i][j] != 0){
-            throw new IllegalStateException("please make board empty");}}}
-       return countSolutionsH(0, 0, 0, 0, 0);
-     }
-     public int find(int r, int c){
-       for(int i = 0; i < board.length; i++){
-         if(board[i][c] == -1){
-           return i;}}
-      return -1;}
-     public int countSolutionsH(int r, int c, int counter, int startOff, int placed){
-       if(placed == board.length){
-         // if all queens
-         edit(startOff, c-1, false);
-         // remove queens in last and previous column
-         // move queen down one in previous column
-         return countSolutionsH(startOff + 1, c - 1, counter += 1, find(r, c-2), 0);}
-       if(r == board.length && c == 0){
-         return counter;}
-       if(r >= board.length){
-         // if can't add queen to column
-         edit(startOff, c - 1, false);
-         // clear previous column
-         //try adding to next row of that column
-         if(c >= 2){
-           return countSolutionsH(startOff + 1, c - 1, counter, find(r, c - 2), placed - 1);}
-
-         if(c == 1){
-           return countSolutionsH(startOff + 1, 0, counter, 0, placed - 1);}}
-       if(board[r][c] == 0){
-         // if no intersections add queen
-         edit(r, c, true);
-         return countSolutionsH(0, c  + 1, counter, r, placed + 1);}
-       return countSolutionsH(r + 1, c, counter, startOff, placed);}
+  public int countSolutions(){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] != 0){
+          throw new IllegalArgumentException("please enter an empty board and try again");}}}
+    countSolutionsH(0);
+    return counter;}
+  public void countSolutionsH(int c){
+    // if you reached end of board add one
+    if(c == board.length){
+      counter ++;
+    return;}
+    for(int i = 0; i < board.length; i++){
+      // if free space add a queen
+      if(board[i][c] == 0){
+        edit(i, c, true);
+        //move to next column
+        countSolutionsH(c + 1);
+        // backtrack and try row underneath
+        edit(i, c, false);}}
+  }
 
   public boolean solveH(int r, int c, int placed){
     // records queen position of previous column
